99135717327331e9a53f3855701c650cf851605c
gg2529 <gg2529@columbia.edu>
2015-05-17 02:37:39 -0400

    Tuple file

--- main.m.cpp
+++ main.m.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
 #include "a_class.h"
+#include <tuple>
+#include <string>
 
+static void tupleCode()
+{
+	std::tuple<int, int, string> a_tuple(10, 11, "First Tuple");
+	std::cout << std::get<1>(a_tuple) << std::endl;
+}
 using namespace std;
 int main()
 {
 std::cout << "Hello world" << std::endl;
-
+/*
 A a;
 a.addValuetoVector(1);
 a.addValuetoVector(2);
@@ -13,6 +20,7 @@ a.addValuetoVector(3);
 a.addValuetoVector(4);
 
 a.printVector();
-
+*/
+tupleCode();
 return 0;
 }
